75adadc24de841440417f11eb502555030c1bd68
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-09 16:47:05 +0200

    fix_bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,60 @@ usando esclusivamente due input e un bottone (non stilizzati),
 realizziamo le specifiche scritte sopra. La risposta finale (o output)
 sarà anch’essa da scrivere in console.
 */
+
+
+const membersNameInputEl = document.getElementById('memberName')
+
+const formOneEl = document.getElementById('formOne')
+console.log(formOneEl);
+
+const cardsEl = document.querySelector('.cardBody')
+const letPrice = 0.21;
+let finalPrice = letPrice * memberKm;
+
+formOneEl.addEventListener('submit', function(e){
+e.preventDefault()
+
+const memberKm = e.target.memberKm.value
+const memberAge = e.target.memberAge.value
+const memberName = e.target.memberName.value
+
+console.log(memberAge, memberKm, memberName);
+
+
+
+const memberEl = `
+<div class"member">
+ <div class="col-4>
+            <h4>nome passeggero</h4>
+            <p class="name_utente"></p>
+        </div>
+        <div class="col-2">
+            <h4>Offerta</h4>
+            <p>Biglietto Standard</p>
+        </div>
+        <div class="col-2">
+            <h4>carrozza</h4>
+            <p>5</p>
+        </div>
+        <div class="col-2">
+            <h4>codice cp</h4>
+            <p>56433</p>
+        </div>
+        <div class="col-2">
+            <h4>costo biglietto</h4>
+            <p class="price"></p>
+        </div>
+            
+</div>
+
+`
+
+cardsEl.insertAdjacentElement('beforeend', memberEl)
+
+})
+
+
+
+
+
